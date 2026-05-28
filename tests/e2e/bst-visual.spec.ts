5ab9e70a0b5c48b229910a70fcf569724c4b9de6
naksh-atra <nakshatra.rajput@outlook.com>
2026-05-28 11:31:15 +0000

    test(bst): add e2e visual spec for bst visualizer

--- tests/e2e/bst-visual.spec.ts
+++ tests/e2e/bst-visual.spec.ts
@@ -2,9 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test.describe('bst visualizer', () => {
   test('visual regression: bst default preset', async ({ page }) => {
-    await page.goto('/topics/binary-search-tree');
+    await page.goto('/OpenCS/topics/binary-search-tree');
     await page.waitForTimeout(2000);
-    // Take screenshot of the visualizer container
-    await expect(page.locator('.bst-visualizer')).toHaveScreenshot('bst-default.png');
+    await expect(page.locator('main article')).toHaveScreenshot('bst-default.png');
   });
 });
